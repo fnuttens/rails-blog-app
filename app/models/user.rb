210e257bd8ae5b30a1564e677201c27667711caf
NUTTENS Florent <florent.nuttens@e-i.com>
2020-11-20 15:30:19 +0100

    feat: configure sendgrid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, if: :email
+
+    after_create :send_greetings
+
+    def send_greetings
+        UserMailer.welcome_email(self).deliver_now
+    end
 end
